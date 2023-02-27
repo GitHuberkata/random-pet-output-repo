777856fe87080382a4b5f5e4dd841e030586843f
GitHuberkata <petyagavrilova86@gmail.com>
2023-02-27 14:14:24 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+erraform {
+
+  cloud {
+    organization = "petya-business-org"
+
+    workspaces {
+      name = "state-remote-random-pet"
+    }
+  }
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "random" {
+}
+
+variable "check" {}
+
+resource "random_pet" "server" {
+  keepers = {
+    # Generates a new pet name on each apply
+    value = var.check  
+  }
+}
